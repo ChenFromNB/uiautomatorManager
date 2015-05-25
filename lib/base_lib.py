46387576ebaa83c12f1e308fa8192cc32aa40cca
chendi <chendi58@gmail.com>
2015-05-25 18:27:33 +0800

    feature: 修改logcat获取设备log的方法，相对简洁

--- lib/base_lib.py
+++ lib/base_lib.py
@@ -13,8 +13,6 @@ class BaseClient(object):
         self._logcat_thread = threading.Thread(target=self._start_logcat)
         self._logcat_thread.start()
         self._device = Device(self.udid)
-        # self._logcat = LogcatThread(self.udid)
-        # self._logcat.start()
 
     def open_app(self):
         self._device.screen.on()
@@ -25,25 +23,9 @@ class BaseClient(object):
 
     def _start_logcat(self):
         log_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'log'))
-        self._adb_commander.cmd('logcat -s %s -c' % self.udid)
-        self._adb_commander.cmd('logcat -s %s > %s/%s.log' % (self.udid, log_file_path, self.udid))
+        self._adb_commander.cmd('-s %s logcat -c' % self.udid)
+        self._adb_commander.cmd(' -s %s logcat> %s/%s.log' % (self.udid, log_file_path, self.udid))
 
-    def __del__(self):
-        pid_name = "adb logcat"
-        os.system("pkill %s" % pid_name)
-
-
-class LogcatThread(threading.Thread):
-    def __init__(self, udid):
-        threading.Thread.__init__(self)
-        self.udid = udid
-        self.daemon = True
-
-    def run(self):
-        _adb_commander = Adb(self.udid)
-        log_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'log'))
-        _adb_commander.cmd('-s %s logcat -c' % self.udid)
-        _adb_commander.cmd('-s %s logcat > %s/%s.log' % (self.udid, log_file_path, self.udid))
-
-    def stop(self):
-        self._is_stopped = True
+    # def __del__(self):
+    #     pid_name = "adb logcat"
+    #     os.system("pkill %s" % pid_name)
